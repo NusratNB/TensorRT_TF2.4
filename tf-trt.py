7d37d9bca85e2a3f089031f619c9c1f9b3e694c5
NusratNB <nusrat.bbismoilov@gmail.com>
2021-04-28 22:24:11 +0900

    TF-TRT_small_project.ipynb notebook has been added. tf-trt.py keeping updated...

--- tf-trt.py
+++ tf-trt.py
@@ -11,3 +11,17 @@ def save_tf_model(path_to_model, path_to_saving):
     tf.saved_model.save(model, path_to_saving)
 
 
+def load_saved_model(saved_model_dir):
+    print("Loading saved model from {} path ...".format(saved_model_dir))
+    model = tf.saved_model.load(saved_model_dir, tags=[tag_constants.SERVING])
+    return model
+
+
+def tf_to_trt_graph(precision_mode="float32", saved_model_dir=None):
+
+    if precision_mode == "float32":
+        precision_mode = trt.TrtPrecisionMode.FP32
+    elif precision_mode == "float16":
+        precision_mode = trt.TrtPrecisionMode.FP16
+
+    out_save_dir = os.path.join(saved_model_dir, )
